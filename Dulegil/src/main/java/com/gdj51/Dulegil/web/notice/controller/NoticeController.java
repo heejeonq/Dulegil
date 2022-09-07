aa18a29b1f8c3c1b2f12807c4b522e3ffebf91a4
조수영 <josuyong96@gmail.com>
2022-09-07 17:15:13 +0900

    notice 컨트롤러 수정

--- Dulegil/src/main/java/com/gdj51/Dulegil/web/notice/controller/NoticeController.java
+++ Dulegil/src/main/java/com/gdj51/Dulegil/web/notice/controller/NoticeController.java
@@ -1,5 +1,17 @@
 package com.gdj51.Dulegil.web.notice.controller;
 
-public class NoticeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+public class NoticeController {
+	
+	@RequestMapping(value="/notice")
+	public ModelAndView aob(ModelAndView mav) {
+		
+		mav.setViewName("notice/notice_list.jsp");
+		
+		return mav;
+	}
 }
