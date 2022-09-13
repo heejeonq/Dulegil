dd1b2eee1afe1eaca9f1c8246b722a765ab3c48d
조수영 <josuyong96@gmail.com>
2022-09-13 15:21:26 +0900

    Merge branch 'main' of https://github.com/heejeonq/Dulegil.git into main

--- Dulegil/src/main/java/com/gdj51/Dulegil/web/mypage/controller/MypageInfoController.java
+++ Dulegil/src/main/java/com/gdj51/Dulegil/web/mypage/controller/MypageInfoController.java
@@ -1,5 +1,42 @@
 package com.gdj51.Dulegil.web.mypage.controller;
 
-public class MypageInfoController {
+import java.util.HashMap;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gdj51.Dulegil.web.dao.IDao;
+
+@Controller
+public class MypageInfoController {
+	@Autowired
+	public IDao dao;
+	
+	@RequestMapping(value = "/mypage_myinfo")
+	public ModelAndView mypage_myinfo(HttpSession session, @RequestParam HashMap<String, String> params, ModelAndView mav) throws Throwable {
+
+		//로그인 안했을때 마이페이지 클릭했을 때 로그인 페이지로
+		if(session.getAttribute("sMemNm") == null && session.getAttribute("sMemNm") == "") {
+			
+			mav.setViewName("login/login");		
+		}
+		else {
+			
+			HashMap<String, String> data = dao.getMap("member.getMyinfo", params);
+			
+			mav.addObject("data", data);
+			
+			mav.setViewName("mypage/mypage_myinfo");
+			
+		}
+
+		
+		return mav;
+
+	}
 }
